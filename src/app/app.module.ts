77bbe8b535b66a947e01e9cae6ef3346cb6cd85b
jhschier <jhschier30@hotmail.com>
2023-12-14 18:33:11 +0000

    header, footer and container styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HomeComponent } from './components/pages/home/home.component';
     FooterComponent,
     AboutComponent,
     HomeComponent,
+    NewMomentComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
